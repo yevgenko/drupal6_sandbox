f3141c06e8fc3307dff3d4d8950191a96b9a4612
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-08-31 22:56:15 +0300

    fix pathes to default assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :default_stage, "staging"
 # To set other stuff use environment related files, i.e.
 # deploy/staging.rb or deploy/production.rb
 
-set :asset_folders, %W(images files)
+set :asset_folders, %W(files)
 
 namespace :deploy do
   desc "Deploy the Drupal"
@@ -13,20 +13,20 @@ namespace :deploy do
     update
     deploy.customize
   end
- 
+
   desc "Setup a GitHub-style deployment."
   task :setup, :except => { :no_release => true } do
     run "git clone #{repository} #{current_path}"
     asset_folders.each do |asset|
-      run "mkdir -p #{shared_path}/#{asset}"
+      run "mkdir -p #{shared_path}/assets/#{asset}"
     end
   end
- 
+
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
     run "cd #{current_path}; git fetch origin; git reset --hard #{branch}; git submodule init && git submodule update"
   end
- 
+
   namespace :rollback do
     desc "Rollback a single commit."
     task :code, :except => { :no_release => true } do
@@ -42,8 +42,8 @@ namespace :deploy do
   desc "Override deploy:symlink task"
   task :symlink, :except => { :no_release => true } do
     asset_folders.each do |asset|
-      run "rm -rf #{current_path}/drupal/#{asset}"
-      run "ln -nfs #{shared_path}/#{asset} #{current_path}/drupal/#{asset}"
+      run "rm -rf #{current_path}/sites/default/#{asset}"
+      run "ln -nfs #{shared_path}/assets/#{asset} #{current_path}/sites/default/#{asset}"
     end
   end
 
@@ -53,7 +53,7 @@ namespace :deploy do
   task :customize, :except => { :no_release => true } do
     # custom tasks
     stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-    theme_asset_paths = %w(images css yaml).map { |p| "#{current_path}/sites/all/themes/*/#{p}" }.join(" ")
+    theme_asset_paths = %w(images css).map { |p| "#{current_path}/sites/all/themes/*/#{p}" }.join(" ")
     run "find #{theme_asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
   end
 end
