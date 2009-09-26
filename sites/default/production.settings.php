bb99ff05d30a79b7bbe661fa290eb86d9964bfe6
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-09-26 05:12:52 +0300

    settings patch, recommended for production env

---
+++ sites/default/production.settings.php
@@ -0,0 +1,15 @@
+/**
+ * Settings patch for production server
+ */
+$conf = array(
+  'cache' => '1',
+  'cache_lifetime' => '0',
+  'error_level' => '0',
+  'preprocess_css' => '1',
+  'watchdog_clear' => '1209600',
+  'javascript_aggregator_aggregate_js' => '1',
+  'javascript_aggregator_optimize_js' => '1',
+  'clean_url' => '1',
+  'xmlsitemap_engines_submit' => 0,
+  'xmlsitemap_engines_cron_submit_frequency' => '86400',
+);
